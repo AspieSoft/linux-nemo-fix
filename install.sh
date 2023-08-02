1e8f9bd4ac83e1c2f007a7263a5b81add6c2e5f8
AspieSoft <aspiesoftweb@gmail.com>
2023-08-02 01:04:32 -0400

    first commit

--- install.sh
+++ install.sh
@@ -5,12 +5,11 @@ dir="$PWD"
 
 sudo echo
 
-sudo mkdir -p /etc/nemo-fix
-sudo cp -rf "./nemo-fix/*" /etc/nemo-fix
+sudo cp -rf "./nemo-fix" "/etc"
 sudo cp -f ./nemo-fix.service "/etc/systemd/system"
 sudo systemctl daemon-reload
 sudo systemctl enable nemo-fix.service --now
 
 if [[ "$dir" =~ linux-nemo-fix/?$ ]]; then
-  rm -rf "$dir"
+  sudo rm -rf "$dir"
 fi
